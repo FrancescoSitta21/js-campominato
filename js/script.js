8e0962e434a6063defec3d13bb8dff5e23bd0dcd
Francesco <sittafrancesco2020@gmail.com>
2020-06-03 16:27:17 +0200

    Prova

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// il pc genera 16 numeri casuali da 1 a 100
+for (i = 1; i<=16; i++) {
+
+  var numeroPc = fromUpTo(1, 100);
+
+  console.log(numeroPc);
+};
+
+//Creazione di una funzione che generi un numero casuale compreso tra due valori
+
+function fromUpTo(min, max) {
+
+  return Math.floor(Math.random()*(max - min + 1)) + min;
+};
+
+var numeroUtente = prompt('Dammi un mumero da 1 a 100');
+
+//if ( i = 0; i!=numeroUtente; ) {
+//  alert("Errore! Riprova!")
+//}
